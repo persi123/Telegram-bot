05e0b235b72d15f217a41c1543eb9c11ac3551b6
prashant_tongra <prashant@foodmonk.com>
2020-05-19 16:10:20 +0530

    "minor-fix"

--- bot.js
+++ bot.js
@@ -1,33 +1,7 @@
-// require('dotenv').config();
-// process.env.NTBA_FIX_319 = 1;
-//process.env["NTBA_FIX_319"] = 1;
-// const config=require("config");
-// Promise.config({
-//     cancellation: true,
-//   });
 var TelegramBot=require("node-telegram-bot-api");
 var token ="1213790461:AAH8-j4nOsCd-s3ZD5LP_Pv_QtzbcJDL8fA"
 var bot=new TelegramBot(token,{polling:true});
 var request=require("request")
-// process.env.NTBA_FIX_319 = 1;
-// bot.onText(/\/"message"(.+)/,function(msg,match){
-//     var chatId=msg.chat.id;
-//     var echo=match[1];
-//     bot.sendMessage(chatId,echo);
-// })
-// const temprature=()=>{
- 
-//     request(`https://api.weatherbit.io/v2.0/current?city=Delhi,India&key=ff82cc414a2f4cc2a4af566a308b25e7`,function(error,response,body){
-//     if(!error && response.statusCode==200){
-//         var res=JSON.parse(body)
-//         bot.sendMessage(chatId=816160224,"Result:\n"+res.data[0].temp)
-//     }
-// })
-// }
-// setInterval(temprature, 5000);
-
-
-
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
@@ -35,6 +9,7 @@ bot.on('message', (msg) => {
   
     // send a message to the chat acknowledging receipt of their message
     bot.sendMessage(chatId, 'Welcome to a Temprature Bot');
+    //debug error
     bot.on("polling_error", (msg) => console.log(msg));
    
 
