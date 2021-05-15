04a451cac8a856922d77acaae99e393afed89422
persi123 <tongrapersi1@gmail.com>
2021-05-15 12:17:57 +0530

    new-fix

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const {Composer} =require('micro-bot')
 // var TelegramBot=require("node-telegram-bot-api");
-var token ="1213790461:AAH8-j4nOsCd-s3ZD5LP_Pv_QtzbcJDL8fA"
+// var token ="1213790461:AAH8-j4nOsCd-s3ZD5LP_Pv_QtzbcJDL8fA"
 // var bot=new TelegramBot(token,{polling:true});
 var bot =new Composer
 var request=require("request")
