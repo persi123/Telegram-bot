92363a56cd8197f2702104f2cb25276daef4c4ec
persi123 <tongrapersi1@gmail.com>
2021-05-15 13:24:41 +0530

    update

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ bot.start((msg) => {
         if(!error && response.statusCode==200){
             var res=JSON.parse(body)
             const dat=res.centers.map(data=>{if(data.sessions[0].min_age_limit == 18 ){
-                  data.sessions[0].available_capacity != 0?msg.reply(data.address):null
+                  data.sessions[0].available_capacity == 0?msg.reply(data.address):null
                 // bot.sendMessage(chatId, data.sessions[0].available_capacity != 0?data?.address?data.address:null:null)
             }
            
